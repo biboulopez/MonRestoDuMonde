ca7ca36b437a28215020cbc0f83fcf221b476836
Vincent Luycx <vincentluycx@macbook-pro-de-vincent.local>
2018-07-23 16:46:53 +0200

    ajout cells

---
+++ MonrestoDuMonde/Vue/MenuCell.swift
@@ -0,0 +1,13 @@
+//
+//  MenuCell.swift
+//  MonrestoDuMonde
+//
+//  Created by Vincent Luycx on 23/07/2018.
+//  Copyright © 2018 Vincent Luycx. All rights reserved.
+//
+
+import UIKit
+
+class MenuCell: UICollectionViewCell {
+    
+}
