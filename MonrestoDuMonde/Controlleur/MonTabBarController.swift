3c734670c194f952e9409c1f63024c173a3c2c71
Vincent Luycx <vincentluycx@macbook-pro-de-vincent.local>
2018-07-23 15:53:00 +0200

    ajout tabbar

---
+++ MonrestoDuMonde/Controlleur/MonTabBarController.swift
@@ -0,0 +1,18 @@
+//
+//  MonTabBarController.swift
+//  MonrestoDuMonde
+//
+//  Created by Vincent Luycx on 23/07/2018.
+//  Copyright © 2018 Vincent Luycx. All rights reserved.
+//
+
+import UIKit
+
+class MonTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tabBar.barTintColor = GRIS_TRES_FONCE
+        tabBar.tintColor = GRIS_TRES_CLAIR
+    }
+}
---
+++ MonrestoDuMonde/Utilitaires/Constantes.swift
@@ -0,0 +1,14 @@
+//
+//  Constantes.swift
+//  MonrestoDuMonde
+//
+//  Created by Vincent Luycx on 23/07/2018.
+//  Copyright © 2018 Vincent Luycx. All rights reserved.
+//
+
+import UIKit
+
+let NOIR = UIColor.black
+let GRIS_TRES_FONCE = UIColor(red: 30/255, green: 30/255, blue: 30/255, alpha: 1)
+let GRIS_TRES_CLAIR = UIColor(red: 240/255, green: 240/255, blue: 240/255, alpha: 1)
+
